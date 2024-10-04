744ce43e39ca16252210bf8eca8ed777b2d087f9
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2024-10-04 13:31:11 +0530

    Create main.tf

---
+++ prod/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  cloud {
+    organization = "Test-ORG2"
+
+    workspaces {
+      name = "VCS-Workspace"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+
+resource "aws_cloudwatch_metric_alarm" "foobar" {
+  alarm_name                = "terraform-test-memory"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 2
+  metric_name               = "mem_used_percent"
+  namespace                 = "CWAgent"
+  period                    = 120
+  statistic                 = "Average"
+  threshold                 = 50
+  alarm_description         = "This metric monitors memory"
+  
+}
